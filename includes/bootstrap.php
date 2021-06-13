c525e18e2c29148f803e82ac1a1ac75f18b06cc0
megaBraker01 <rafaelperez7461@hotmail.com>
2021-06-14 00:06:58 +0200

    eliminamos los archivos y carpetas que no pertenecen a la libreria

--- includes/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<!-- Bootstrap 4 CSS -->
-<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">-->
-    
-    <!-- Bootstrap 3 CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
